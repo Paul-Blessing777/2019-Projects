1a46aff1103ed02357200e948164e23ccc6efa2a
Paul Blessing <80071381+Paul-Blessing777@users.noreply.github.com>
2024-08-12 07:50:48 -0400

    Delete HAVING clause.sql

--- HAVING clause.sql
+++
@@ -1,9 +0,0 @@
---HAVING clause
---The having clause allows us to filter after an aggregation has taken place
-SELECT customer_id, SUM(amount) FROM payment
-GROUP BY customer_id
-HAVING SUM(amount) > 160;
-
-SELECT store_id, COUNT(customer_id) FROM customer
-GROUP BY store_id
-HAVING COUNT(customer_id) >300
