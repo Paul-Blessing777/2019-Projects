df0678d633749dc0d8639422a83768a626a34c9e
Paul Blessing <80071381+Paul-Blessing777@users.noreply.github.com>
2024-08-12 07:51:58 -0400

    Delete LEFT JOIN.sql

--- LEFT JOIN.sql
+++
@@ -1,8 +0,0 @@
---LEFT OUTER JOIN
---Results in the set of records that are in the left table,
---If there is no match with the right table, the results are null
---Order matters for left joins!
-
-SELECT film.film_id, film.title, inventory_id FROM film
-LEFT JOIN inventory ON inventory.film_id = film.film_id
-WHERE inventory.film_id IS Null
