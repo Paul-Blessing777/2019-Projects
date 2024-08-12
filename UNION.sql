76b2d13a3dff3adbbc24968f5231b2f6532a101b
Paul Blessing <80071381+Paul-Blessing777@users.noreply.github.com>
2024-08-12 07:49:08 -0400

    Delete UNION.sql

--- UNION.sql
+++
@@ -1,8 +0,0 @@
---UNION
---Used to combine the result set of two or more SELECT statements
---Basically serves to directly concatenate two results together
-
-SELECT * FROM Sales2021_Q1
-UNION
-SELECT * FROM Sales2021_Q2
-ORDER BY name_1;
