8d53d89e9feff17ac4c3aeac05ab94cf6d0eb7fa
Paul Blessing <80071381+Paul-Blessing777@users.noreply.github.com>
2024-08-12 07:50:17 -0400

    Delete LIMIT.sql

--- LIMIT.sql
+++
@@ -1,8 +0,0 @@
---LIMIT
---Allows us to limit the # of rows returned for a query
---Useful if you just want to see the top few rows for table.
---Useful in combo with ORDER BY
-SELECT * FROM payment
-WHERE amount <> 0
-ORDER BY payment_date DESC
-LIMIT 5;
